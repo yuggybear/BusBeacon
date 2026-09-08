23ea5987c7c4dbcb798e59307faa7b0f8558f8a7
yuggybear <122220016+yuggybear@users.noreply.github.com>
2026-09-07 20:55:50 -0400

    Fix Vite config import error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import base44 from '@base44/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
